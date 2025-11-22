60efaafdeafa3f71d50d80882b6ab5c86106918f
SA-NG-OC <sang22102005@gmail.com>
2025-11-22 16:24:24 +0700

    first commit

---
+++ OOAD-BE/Fuba-BE/src/main/java/com/example/Fuba_BE/FubaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fuba_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FubaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FubaBeApplication.class, args);
+	}
+
+}
---
+++ OOAD-BE/Fuba-BE/src/test/java/com/example/Fuba_BE/FubaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fuba_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FubaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
